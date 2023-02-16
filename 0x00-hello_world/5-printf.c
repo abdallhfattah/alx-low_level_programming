f6f4a39ae96bfa429a610e1c9dbdec112d6ee79b
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-16 15:18:52 +0200

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
 * Return: Always 0.
 */
 int main(void){
-    printf("with proper grammar, but the outcome is a piece of art,");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
 
