1e2498543f8d7048d17aa68027273acae1d3ebf3
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-16 15:10:57 +0200

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+* main - prints "with proper grammar, but the outcome is a piece of
+* art," followed by a newline
+* Return: Always 0.
+*/
+int main(void){
+    printf("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+
