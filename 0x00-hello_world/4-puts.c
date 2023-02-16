07df6d033cebdbd5f18c8a7846370d4933ccd17b
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-16 14:57:27 +0200

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 
 /*
-* main - prints "Programming is like building a multilingual puzzle
-*  ,Followed by a new line .
-* Return: 0 .
+* main - prints "Programming is like building a multilingual
+* puzzel , followed by a newline .
+* Return: Always 0.
 */
 int main(void){
     puts("\"Programming is like building a multilingual puzzle");
