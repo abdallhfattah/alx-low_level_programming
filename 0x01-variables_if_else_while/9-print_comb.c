9c132f9f0704544b66918a5a3134391472c03109
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-21 15:34:25 +0200

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,18 +9,20 @@
 
 int main(void)
 {
-int n;
+        int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-n = 48;
-while(n < 58){
-putchar(n++);
-if(n != 58){
-        putchar(',');
-        putchar(' ');
-}
-}
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        n = 48;
+        while(n < 58)
+        {
+                putchar(n++);
+                if(n != 58)
+                {
+                        putchar(',');
+                        putchar(' ');
+                }
+        }
 
         putchar('\n');
 	return (0);
