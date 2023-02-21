5c243d445115cb88c97bb64f5cf4f89b6064a8f7
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-21 15:30:45 +0200

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
  * main - Entery point
  * Return: always 0
@@ -15,7 +13,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        /* your code goes there */
         n = 48;
         while(n < 58){
         putchar(n++);
@@ -23,7 +20,7 @@ int main(void)
                 putchar(',');
                 putchar(' ');
         }
-        }
+}
         putchar('\n');
 	return (0);
-}   
+}
