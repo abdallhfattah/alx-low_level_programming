bf024a0db762ef8ade33ac7b56f4fe22d7befb7c
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-21 19:27:12 +0200

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,20 +6,27 @@
  */
 int main(void)
 {
-	int i, j;
-
+	int i, j , n;
+	n = 0;
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-
-			if (i+j < 18)
-			{
-				putchar(',');
+				putchar(i + '0');
+				putchar(j + '0');
+				if(n < 1)
+				{
+						n++;
+				}
+				else
+				{
+					n = 0;
+					if (i+j < 18)
+						{
+							putchar(',');
+						}
+				}
 				putchar(' ');
-			}
 		}
 	}
 
