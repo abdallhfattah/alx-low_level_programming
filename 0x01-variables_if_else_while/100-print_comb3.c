473f0d5ebfa2921fff8264a2aab9c62f475ecc4b
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-21 16:14:13 +0200

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,30 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entery point
- * Return: always 0
+ * main - Entry point
+ * Return: Always 0
  */
-
 int main(void)
 {
-        int i, j;
+	int i, j;
 
-        i = 0;
-        j = 0;
-        for(i = 0; i < 9; i++)
-        {
-                for (j = i + 1; j <= 9; j++)
-                {
-                        putchar(i + '0');
-                        putchar(j + '0');
-                        if(i < 8)
-                        {
-                        putchar(',');
-                        putchar(' ');
-                        }
-                }
-                
-        }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
 
-        putchar('\n');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
 	return (0);
 }
+
