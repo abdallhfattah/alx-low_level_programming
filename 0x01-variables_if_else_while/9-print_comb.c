ced7e04b5428d3f1047c0f3ec69bfa4156e7265a
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-21 15:39:07 +0200

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,13 +16,13 @@ int main(void)
         n = 48;
         while(n < 58)
         {
-                putchar(n++);
-                if(n != 58)
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
+        putchar(n++);
+        if(n != 58)
+        {
+                putchar(',');
+                putchar(' ');
         }
+}
 
         putchar('\n');
 	return (0);
