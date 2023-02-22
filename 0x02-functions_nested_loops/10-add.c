cf56a67fe1ba7aaf805d4024a22fd82a3bd9eb15
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-23 00:27:11 +0200

    ya wala

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,14 @@
-#include<unistd.h>
+#include "main.h"
+
+/**
+ * add - addes the given two numbers
+ * 
+ * @a: parameter
+ * @b: parameter
+ * Return: the sum 
+ */
+
+int add(int a, int b)
+{
+return (a+b);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,42 @@
-#include<unistd.h>
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times the table
+ * Return: no return
+ */
+
+
+void times_table(void)
+{
+
+int a, i, c, d, f;
+for (a = 0; a < 10; a++)
+{
+for (i = 0; i <= 9; i++)
+{
+c = a * i;
+if (c > 9)
+{
+d = c % 10;
+f = (c - d) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(f + '0');
+_putchar(d + '0');
+}
+else
+{
+if (i != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+
+}
+_putchar(c + '0');
+}
+}
+
+_putchar('\n');
+}
+}
