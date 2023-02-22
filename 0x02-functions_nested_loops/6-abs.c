6f98d8c16b78a8d7f02b4b7e79fdcd393c8e4d2e
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-22 20:56:17 +0200

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,19 @@
-#include<unistd.h>
+#include"main.h"
+
+/**
+ * _abs - gets absolute value of any number.
+ * @c: parameter passed
+ * 
+ * Return: the postive value integer.
+ */
+int _abs(int c)
+{
+if(c < 0)
+{
+return (-c);
+}
+else
+{
+return (c);
+}
+}
