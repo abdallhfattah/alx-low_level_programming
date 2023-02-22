7db40b9ff57fb04c2cd77011fc96c8854ca6d636
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-22 15:22:29 +0200

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
-#include<unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    for (int i = 0; i < 26; i++)
+    {
+        _putchar(i + 'a');
+    }
+
+    _putchar('\n');
+    return (0);
+
+}
