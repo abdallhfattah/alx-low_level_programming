bbbc06e8b32080018e1cf4eb86e0ec2db28c6e6f
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-22 20:36:17 +0200

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,15 @@
-#include<unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha - checking the character if its on alphabet or not
+ * @c: is variable to check with ascii code.
+ * Return: 1 if small otherwise 0
+ */
+
+int _isalpha(int c)
+{
+        if ((96 < c && 123 > c) || (64 < c && 91 > c))
+            return (1);
+        else
+            return (0);
+}
