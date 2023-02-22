97bf44e5e96b74befbabdaa9c5e532360c8f1972
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-22 15:39:16 +0200

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,14 @@
-#include<unistd.h>
+#include "main.h"
+
+/**
+ * _islower - checking the character if its small or not
+ * Return: 1 if small otherwise 0
+ */
+
+int _islower(int c)
+{
+    if(96 < c && c < 123)
+        return (1);
+    else
+        return (0);
+}
