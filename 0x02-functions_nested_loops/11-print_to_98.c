db9f911bc8ae70d0ab1c1e7115b80d22c8c5cbdf
abdallhfattah <abdallh.fattah@33gmail.com>
2023-02-23 02:33:01 +0200

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,37 @@
-#include<unistd.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * print_times_tables - printing to 98.
+ * @n: variable to check the number it self. 
+ */
+void print_to_98(int n)
+{
+
+    if (n < 98)
+    {
+        for (; n <= 98; n++)
+        {
+
+            if (n == 98)
+            {
+            printf("%d", n);
+            break;
+            }
+            printf("%d, ", n);
+        }
+    }
+    else{
+        for (;n >= 98; n--)
+        {
+            if (n == 98)
+            {
+            printf("%d", n);
+            break;
+            }
+            printf("%d, ", n);
+        }
+    }
+    printf("\n");
+}
