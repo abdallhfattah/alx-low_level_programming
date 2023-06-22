279a7cca91132e0f8e5f38453afe96871eda0901
abdallhfattah <abdallh.fattah@33gmail.com>
2023-06-22 19:11:01 +0300

    yarab

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,33 +1,12 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-/**
- * main - entry point
- * Description: prints  if a number is positive or negative.
- * Return: 0
- */
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaning_days(int month, int day, int year);
 
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	printf("%d is ", n);
-
-	if (n > 0)
-	{
-		printf("positive\n");
-	}
-	else if (n == 0)
-	{
-		printf("zero\n");
-	}
-	else
-	{
-		printf("negative\n");
-	}
-	return (0);
-}
+#endif /* MAIN_H */
