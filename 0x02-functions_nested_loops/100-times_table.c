de26e4b9a0476239369bebb7f324e52e271f254a
abdallhfattah <abdallh.fattah@33gmail.com>
2023-06-23 11:24:24 +0300

    yarab

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,26 +5,39 @@
 /**
  * print_times_table - printing table of size n + 1
  * Return: no Return value.
- * 
+ *
  */
 void print_times_table(int n)
 {
-    int i , j, z;
-    if (n >= 0 && n <= 15){
+	int i, j, z;
+	if (n >= 0 && n <= 15)
+	{
 
-    for (i = 0; i <= n; i++)
-    {
-        z = 0;
-       for (j = 0; j <= n; j++)
-       {
-        if (i != n || j != n)
-            printf("%d,  ", z);
-        else
-            printf("%d", z);
-        z = z + i;
-       }
-
-    }
-    }
-    
+		for (i = 0; i <= n; i++)
+		{
+			z = 0;
+			for (j = 0; j <= n; j++)
+			{
+				if (i != n || j != n)
+					if (z >= 10 && z < 100)
+					{
+						printf("%d,  ", z);
+					}
+					else if (z >= 100 && z < 1000)
+					{
+						printf("%d, ", z);
+					}
+					else
+					{
+						printf("%d,   ", z);
+					}
+				else
+				{
+					printf("%d", z);
+				}
+				z = z + i;
+			}
+			printf("\n");
+		}
+	}
 }
