5749c2bf0df7c26c97d699f80f17bf22f6572f10
abdallhfattah <abdallh.fattah@33gmail.com>
2023-11-27 21:38:26 +0200

    dog header added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,8 +6,6 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- *
- * Description: Longer description
  */
 struct dog
 {
@@ -16,7 +14,12 @@ struct dog
 	char *owner;
 };
 
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
