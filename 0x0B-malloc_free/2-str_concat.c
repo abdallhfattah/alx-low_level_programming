c820e868f70d111394a1fa29ff6b9ff97cb7a114
abdallhfattah <abdallh.fattah@33gmail.com>
2023-11-30 20:19:30 +0200

    malloc free done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,11 +24,10 @@ char *str_concat(char *s1, char *s2)
 	i = ci = 0;
 
 	while (s1[i] != '\0')
-	{
 		i++;
-		while (s2[ci] != '\0')
-			ci++;
-	}
+
+	while (s2[ci] != '\0')
+		ci++;
 
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
