1af0852e2809d45ebc6bd008b2b6c9a3c4816e53
abdallhfattah <abdallh.fattah@33gmail.com>
2024-01-30 22:01:20 +0200

    hacking jackbot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://github.com/abdallhfattah/alx-low_level_programming/master/0x18-dynamic_libraries
-export LD_PRELOAD="$PWD/../libhack.so"
+export LD_PRELOAD="$PWD/../libgiga.so"
